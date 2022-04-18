854ddf525e9b66f8d1f1bf0cdcfb39a31a4ef3f8
orkhanshirin <orkhan.shirinow@gmail.com>
2022-04-18 14:22:38 +0000

    Added missing type hints

--- src/authk/_authorized_keys.py
+++ src/authk/_authorized_keys.py
@@ -6,7 +6,7 @@ import contextlib
 import os
 import shutil
 from pathlib import Path
-from typing import Final
+from typing import Any, Dict, Final
 
 from sshpubkeys import AuthorizedKeysFile
 
@@ -19,17 +19,17 @@ class AuthorizedKeys:
     Class for handling authorized_keys file.
     """
 
-    def __init__(self):
+    def __init__(self) -> None:
         self._keys = {}
 
-    def __enter__(self):
+    def __enter__(self) -> Dict[str, Any]:
         with contextlib.suppress(FileNotFoundError):
             authk = AuthorizedKeysFile(open(_FILE_NAME, encoding="utf-8"))
             for key in authk.keys:
                 self._keys[key.comment] = key
         return self._keys
 
-    def _create_ssh_dir(self, ssh_dir):
+    def _create_ssh_dir(self, ssh_dir: str) -> str:
         if not os.path.exists(ssh_dir):
             os.mkdir(ssh_dir)
             os.chmod(ssh_dir, 0o700)
@@ -38,18 +38,18 @@ class AuthorizedKeys:
             print(f"Missing directory created at {ssh_dir}")
         return "Success"
 
-    def _change_file_ownership(self, file_name):
+    def _change_file_ownership(self, file_name: str) -> None:
         file_info = Path(file_name)
         shutil.chown(file_name, file_info.owner(), file_info.group())
         os.chmod(file_name, 0o600)
 
-    def _create_or_update_authoried_keys(self, file_name):
+    def _create_or_update_authoried_keys(self, file_name: str) -> None:
         with open(file_name, "a", encoding="utf-8") as file:
             payload = "\n".join([str(key.keydata) for key in self._keys.values()])
             file.write(payload)
         self._change_file_ownership(file_name)
 
-    def __exit__(self, exception_type, exception_value, traceback):
+    def __exit__(self, exception_type, exception_value, traceback) -> None:
         self._create_ssh_dir(_SSH_DIR)
         self._create_or_update_authoried_keys(_FILE_NAME)
         if all([exception_type, exception_value, traceback]):
