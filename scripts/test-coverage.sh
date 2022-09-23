ea70872c2cc9ab84d25ce68761adac8da3ec22b4
orkhanshirin <orkhan.shirinow@gmail.com>
2022-09-23 09:03:34 +0000

    Fixing the test-coverage check: iteration 4

--- scripts/test-coverage.sh
+++ scripts/test-coverage.sh
@@ -4,4 +4,4 @@ set -x
 python3 -m pip install -U pip --quiet
 pip install flake8 pytest pytest-cov --quiet
 
-pytest --cache-clear --no-cov-on-fail --cov=src/authk test/unit/
+pytest --cache-clear --no-cov-on-fail --cov=src/authk test/unit/ 
